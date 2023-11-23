de6db86ae2783ae37c3bd63a46f6887da29f61ac
NosovaValeria <Tyuiu.NosovaVD@outlook.com>
2023-11-23 16:07:31 +0500

    Task7

---
+++ Tyuiu.NosovaVD.Sprint5.Task7.V17/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.NosovaVD.Sprint5.Task7.V17.Lib;
+namespace Tyuiu.NosovaVD.Sprint5.Task7.V17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Носова В.Д. | ПКТб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #17                                                             *");
+            Console.WriteLine("* Выполнил: Носова Валерия Дмитриевна | ПКТб-23-2                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл С:\\DataSprint5\\InPutDataFileTask7V17.txt. в котором есть набор *");
+            Console.WriteLine("* символьных данных. Удалить все удвоенные буквы \"нн\" из файла. Полученный*");
+            Console.WriteLine("* результат сохранить в файл OutPutDataFileTask7V17.txt.                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            string path = @"C:\DataSprint5\InPutDataFileTask7V17.txt";
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Находится в файле: ");
+            Console.WriteLine(ds.LoadDataAndSave(path));
+            Console.ReadKey();
+        }
+    }
+}
