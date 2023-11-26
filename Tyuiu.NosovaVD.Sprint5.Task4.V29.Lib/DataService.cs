62119125c6905c9a6e8090213104d8848bbeb7ce
NosovaValeria <Tyuiu.NosovaVD@outlook.com>
2023-11-26 13:06:09 +0500

    Task4

--- Tyuiu.NosovaVD.Sprint5.Task4.V29.Lib/DataService.cs
+++ Tyuiu.NosovaVD.Sprint5.Task4.V29.Lib/DataService.cs
@@ -12,6 +12,7 @@ namespace Tyuiu.NosovaVD.Sprint5.Task4.V29.Lib
         public double LoadFromDataFile(string path)
         {
             string strX = File.ReadAllText(path);
+            strX = strX.Replace('.', ',');
             double res = Math.Round(((Convert.ToDouble(strX) / (2.0 * Convert.ToDouble(strX))) + Math.Sin(Math.Pow(Convert.ToDouble(strX), 2))), 3);
             return res;
         }
